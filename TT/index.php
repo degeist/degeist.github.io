6af057e788928abe38b19d3c00f78b7f8c7b81eb
André Avorio <a@avor.io>
2015-09-07 18:03:45 +0100

    Copy of the avorio/degeist.com repository

---
+++ TT/index.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <title>TT</title>
+		<meta name="author" content="De Geist, London">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <!-- Bootstrap -->
+    <link href="css/bootstrap.min.css" rel="stylesheet">
+		<link href="css/style.css" rel="stylesheet">
+		
+    <!-- HTML5 Shim and Respond.js IE8 support of HTML5 elements and media queries -->
+    <!-- WARNING: Respond.js doesn't work if you view the page via file:// -->
+    <!--[if lt IE 9]>
+      <script src="https://oss.maxcdn.com/libs/html5shiv/3.7.0/html5shiv.js"></script>
+      <script src="https://oss.maxcdn.com/libs/respond.js/1.3.0/respond.min.js"></script>
+    <![endif]-->
+
+	<!-- Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=PT+Sans|Montserrat:400,700' rel='stylesheet' type='text/css'>
+	<link href="//netdna.bootstrapcdn.com/font-awesome/4.0.3/css/font-awesome.css" rel="stylesheet">
+	
+	
+	
+	<!-- Fav and touch icons -->
+	<link rel="shortcut icon" href="img/favicon.ico">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/dg-icon-144.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/dg-icon-114.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/dg-icon-72.png">
+	<link rel="apple-touch-icon-precomposed" href="img/dg-favicon.png">	
+	
+	
+	<!-- Open Graph -->
+	<meta property="og:image" content="LOGO URL" />
+	<meta property="og:title" content="TT" />
+	<meta property="og:description" content="FACEBOOK TEXT." />
+	<meta property="og:url" content="URL" />	
+	</head>
+  
+	<body>
+		<div id="wrap">
+			
+			<div class="header">
+				<img src="img/TT-logo.png" alt="TT" class="logo" />
+				
+				<nav>
+					<ul id="menu">
+						<li><a href="#p1" id="about"><i class="fa fa-asterisk"></i></a></li>
+						<li><a href="#p2" id="jobs"><i class="fa fa-road"></i></a></li>
+						<li><a href="#p3" id="contact"><i class="fa fa-envelope-o"></i></a></li>
+					</ul>
+				</nav>
+			</div><!-- /.header -->	
+		
+			<div class="container">
+					
+					<div id="p1" class="main">
+						<h1>Lorem Ipsum</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</div> <!-- /.main-->
+					
+					<div id="p2" class="main">
+						<h1>Jobs</h1>
+						<p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+					</div> <!-- /.main-->
+					
+					<div id="p3" class="main">
+						<h1>Contact</h1>
+						<p>Lorem ipsum dolor sit amet,.</p>
+					</div>		<!-- /.main-->
+					
+			</div><!-- /.container -->		
+
+		</div><!-- /#wrap -->				
+	
+		<div class="footer">
+			<div class="container">
+				<p><span class="symbol">&copy;</span> 2014</p>
+			</div><!-- /.container -->		
+		</div><!-- /.footer -->		
+
+    <!-- jQuery (necessary for Bootstrap's JavaScript plugins) -->
+    <script src="https://code.jquery.com/jquery.js"></script>
+    <!-- Include all compiled plugins (below), or include individual files as needed -->
+    <script src="js/bootstrap.min.js"></script>
+		<script src="js/scripts.js"></script>
+  </body>
+</html>
---
+++ index.php
@@ -0,0 +1,232 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">   
+	<meta name="author" content="De Geist, London">
+	<title>De Geist | The Digital Spirit</title>
+	 <meta name="description" content="We think about and create Internet things. We can help you make the most out of the networked world.">	
+	<!-- Fonts -->
+	<link href='http://fonts.googleapis.com/css?family=Josefin+Slab:400,700|Fauna+One' rel='stylesheet' type='text/css'> 
+    <!-- CSS --> 
+	<link href="css/bootstrap.css" rel="stylesheet" type="text/css"> 
+	<link href="css/screen.css" rel="stylesheet" type="text/css"> 
+	<link href="css/dg-icons.css" rel="stylesheet" type="text/css">
+	<!-- HTML5 shim, for IE6-8 support of HTML5 elements <link href="//netdna.bootstrapcdn.com/font-awesome/3.2.1/css/font-awesome.min.css" rel="stylesheet">  -->
+	<!--[if lt IE 9]>
+		<script src="http://html5shim.googlecode.com/svn/trunk/html5.js"></script>
+	<![endif]-->
+	
+	<!-- Fav and touch icons -->
+	<link rel="shortcut icon" href="img/favicon.ico">
+	<link rel="apple-touch-icon-precomposed" sizes="144x144" href="img/dg-icon-144.png">
+	<link rel="apple-touch-icon-precomposed" sizes="114x114" href="img/dg-icon-114.png">
+	<link rel="apple-touch-icon-precomposed" sizes="72x72" href="img/dg-icon-72.png">
+	<link rel="apple-touch-icon-precomposed" href="img/dg-favicon.png">	
+	
+	
+	<!-- Open Graph -->
+	<meta property="og:image" content="http://www.degeist.com/img/dg-string-logo-sm.png" />
+	<meta property="og:title" content="De Geist" />
+	<meta property="og:description" content="We think about and create Internet things." />
+	<meta property="og:url" content="http://www.degeist.com" />	
+
+	
+	<script src="//ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<script type="text/javascript" src="js/modernizr.custom.js"></script>
+	<script type="text/javascript" src="js/script.js"></script>
+</head>
+<body>
+	<!-- analytics -->
+	<script type="text/javascript">
+
+		var _gaq = _gaq || [];
+		_gaq.push(['_setAccount', 'UA-19437887-1']);
+		_gaq.push(['_trackPageview']);
+
+		(function() {
+			var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true;
+			ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js';
+			var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
+		})();
+
+	</script>
+
+	<!-- #stickynav -->
+	<div data-spy="affix" data-offset-bottom="100" id="stickynav" class="pull-left col-lg-1">
+		<ul>
+			<li title="Welcome" class="active"><a href="#Welcome" title="Top"><span aria-hidden="true" class="icon-dg"></span></a></li>
+			<li title="Services"><a href="#Services" title="Services"><span aria-hidden="true" class="icon-services"></span></a></li>
+			<li title="People"><a href="#People"><span aria-hidden="true" class="icon-asterisk1a"></span></a></li>
+			<li title="Contact"><a href="#Contact"><span aria-hidden="true" class="icon-contact"></span></a></li>
+		 </ul>
+	</div>
+	<!-- /#stickynav -->
+	<!-- .top-banner -->
+	<div class="top-banner">
+		<section id="Welcome" class="sec pull-center">
+			<!-- <img src="img/bg2.jpg" alt="" class="banner-bg"> -->
+			<div class="container">
+				<a class="brand" href="/"><?php include('img/dg-string-logo.svg'); ?></a>
+				
+				<div class="caption">		
+					<h1>The Digital Spirit</h1>
+					<p>We think about and create Internet things. <br />We can help you make the most out of the networked world.</p>		
+					
+				</div> <!-- /.caption -->
+			</section>
+			
+				<div class="next-section">
+					<a href="#Services">
+						<span>What we do</span>
+							<div class="fs1" aria-hidden="true" data-icon="&#xe005;"></div>	
+					</a>
+				</div>
+			</div> <!-- /.container -->
+		</div> <!-- /.top-banner -->
+
+		<!-- .main -->		
+		<div class="main">
+			<div class="container">
+				<section id="Services" class="sec pull-center">					
+				<h2 class="featurette-heading"><span aria-hidden="true" class="icon-services"></span><br />Services</h2>
+					
+					<!-- 3 columns <p><a class="bttn" href="#">Read more</a></p> -->
+					<div class="row main-boxes">
+							 <div class="col-md-4 box"> 							
+								<?php include("img/dg-bulb.svg"); ?>
+								<h2>Think</h2>
+								<p>The Internet is at the core of our social and commercial interactions. However, sometimes it can be chaotic out there. With years of experience writing about and analysing the Internet, we can help you navigate the chaos and point you towards digital enlightenment.</p>
+
+							</div> <!-- /.span4 -->
+							
+							<div class="col-md-4 box"> 
+								<?php include("img/pencil-icon.svg"); ?>
+								<h2>Plan</h2>
+								<p>With a strong emphasis on human-centered design and systems thinking, we distil insights into real and practical digital strategies. As a boutique digital consultancy, we identify opportunities and challenges and advise on how to make the most of the networked world.</p>
+							</div> <!-- /.span4 -->
+							
+							<div class="col-md-4 box"> 
+								<?php include("img/wrench.svg"); ?>
+								<h2>Create</h2>
+								<p>It’s all in the execution. A good idea has value when manifested in a user-friendly and efficient solution. To build the best products, we collaborate with the appropriate talent on a project-by-project basis, oversee the development and help you launch it.</p>							
+							</div> <!-- /.span4 -->
+						</div> <!-- /.row -->					
+				</section>
+			
+				<!-- /.next section -->		
+				<div class="next-section">
+					<a href="#People">
+						<span>Who we are</span>
+							<div class="fs1" aria-hidden="true" data-icon="&#xe005;"></div>
+					</a>
+				</div>
+				
+			</div><!-- /.container -->		
+		</div><!-- /.main -->		
+		
+		<!-- .people -->
+		<div class="people">
+			<section id="People" class="sec pull-center">
+				<div class="container">
+					<h2 class="featurette-heading"><i class="icon-asterisk"></i><br />People</h2>
+					<div class="row">	
+						<div class="col-lg-4 col-lg-offset-1 col-sm-6 box">
+							<img class="img img-responsive" src="img/andre-avorio.png" alt="Andre Avorio">
+							<h2 class="no-space">Andre Avorio</h2>
+							<h3 class="subtitle"> - Digital Architect - </h3>
+							<p>André specialises in the intersection of business strategy and digital technology. With a focus on user experience, he follows ideas from conception through development to real digital products.</p>
+							<div class="row social-media">																
+								<a href="http://www.linkedin.com/in/avorio/"><span aria-hidden="true" class="icon-linkedin"></span></a>	
+								<a href="https://twitter.com/avorio"><span aria-hidden="true" class="icon-twitter"></span></a>	
+								<a href="https://github.com/avorio"><span aria-hidden="true" class="icon-github"></span></a>
+								<a href="http://www.flickr.com/photos/avorio"><span aria-hidden="true" class="icon-flickr"></span></a>	
+							</div>
+						</div><!-- /.people-box --> 	
+										
+						<div class="col-lg-4 col-lg-offset-2 col-sm-6 box">
+							<img class="img img-responsive" src="img/ras-hagen.png" alt="Rasmus Hagen">
+							<h2 class="no-space">Rasmus Hagen</h2>
+							<h3 class="subtitle"> - Design Creative - </h3>
+							<p>Rasmus has many years of online experience as a creator and consultant. He is powered by a creative chaos that is distilled in an accessible - and undeniably Scandinavian - minimalism.</p>
+							<div class="row social-media">
+								<a href="http://www.behance.net/rashagen"><span aria-hidden="true" class="icon-behance"></span></a>
+								<a href="http://www.linkedin.com/in/rasmushagen/"><span aria-hidden="true" class="icon-linkedin"></span></a>	
+								<a href="http://www.flickr.com/photos/hagenmus"><span aria-hidden="true" class="icon-flickr"></span></a>	
+								<a href="https://twitter.com/rasmushagen"><span aria-hidden="true" class="icon-twitter"></span></a>	
+								<a href="https://github.com/rashagen"><span aria-hidden="true" class="icon-github"></span></a>
+							</div>
+						</div><!-- /.span4 --> 										
+					</div>  
+				</div>	<!-- /.container & /.row -->
+			</section>
+			<!-- /.next section -->		
+			<div class="next-section">
+				<a href="#Contact">
+					<span>Get in touch</span>
+					<div class="fs1" aria-hidden="true" data-icon="&#xe005;"></div>
+				</a>
+			</div>
+		</div>
+		
+    <!-- Contact <hr class="featurette-divider"> -->
+		<div class="contact">
+			<div class="container">
+				<section id="Contact" class="sec pull-center">			
+						<div class="col-md-offset-3 col-md-6">
+							<form>	
+								<h2 class="featurette-heading"><span aria-hidden="true" class="icon-contact"></span><br />Contact us</h2>
+								<h3 class="subtitle"><span class="emph">Ask us anything</span></h3>
+								
+									<div class="front-contact-form" id="contact-form">
+										<label>Email</label>
+										<input type='text' name='email' placeholder='name@email.com' id="email" />
+										<label class='error' id='email_error'>Please leave a valid email</label>									
+									
+										<label>Message</label>
+										<textarea placeholder='Your message' name='message' id="message"></textarea>
+										<label class='error' id='message_error'>Forgot to leave us a message</label>
+									
+										<div id="loading"><h3 class="sending"><span aria-hidden="true" class="icon-paperplane"></span> Sending</h3></div>
+									
+										<input type='submit' value='send' class='submit bttn send' />
+
+									</div>		
+							</form> 
+						</div>
+				</section>
+			</div> <!-- /.container -->
+		</div> 	<!-- /.contact -->
+
+    <!-- FOOTER -->
+    <div id="footer">
+			<div class="container">
+				<div class="row connect-icons">	  			
+					<div class="">
+							<div class="col-xs-3 col-md-offset-4 col-md-1">
+								<a href="mailto:hello@degeist.com?Subject=Hello%20De%20Geist" class ="email"><span aria-hidden="true" class="icon-paperplane"></span></i></a>
+							</div>	
+							<div class="col-xs-3 col-md-1">
+								<a href="http://www.linkedin.com/company/de-geist/" class ="linkedin"><span aria-hidden="true" class="icon-linkedin"></span></a>
+							</div>
+							<div class="col-xs-3 col-md-1">
+								<a href="https://twitter.com/degeist" class ="twitter"><span aria-hidden="true" class="icon-twitter"></span></a>
+							</div>
+							<div class="col-xs-3 col-md-1">
+								<a href="https://github.com/degeist" class ="github"><span aria-hidden="true" class="icon-github"></span></a>
+							</div>									
+						</div>			
+				</div>
+				<div class="row">
+						<p class="text-center">&copy; 2013 De Geist</p>
+				</div>
+			</div>
+    </div>
+
+    <!-- Load javascript -->
+    <script type="text/javascript" src="js/bootstrap.js"></script>
+		<script type="text/javascript" src="js/custom.js"></script>	
+    <script type="text/javascript" src="js/holder.js"></script>
+  </body>
+</html>
